63877ce5fe537a6ba9f9267a167d765472422e00
Davide Boschetti <bdavide1999@gmail.com>
2025-04-27 19:46:33 +0200

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,5 @@
-const CACHE_NAME = 'CuCu-PWA-v1';
+const CACHE_NAME = 'CuCu-PWA-v2';
 const urlsToCache = [
-    './',
-    './index.html',
     './styles.css',
     './script.js',
     './manifest.json',
@@ -18,16 +16,26 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', event => {
+    if (event.request.mode === 'navigate') {
+        return event.respondWith(fetch(event.request).catch(() => caches.match('index.html')));
+    }
     event.respondWith(
-        fetch(event.request)
-            .then(response => {
-                return caches.open(CACHE_NAME).then(cache => {
-                    cache.put(event.request, response.clone());
-                    return response;
-                });
-            })
-            .catch(() => {
-                return caches.match(event.request);
-            })
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
     );
 });
