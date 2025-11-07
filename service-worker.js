64ecb0fb0149734b1aca3e3c8267bad8c7d9194d
Davide Boschetti <bdavide1999@gmail.com>
2025-11-07 03:41:51 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // === CuCu SW: cambia SOLO questa riga per forzare un refresh completo ===
-const CACHE = 'cucu-7-11-2025';
+const CACHE = 'cucu-2-11-2025';
 // ========================================================================
 
 // Asset principali da mettere in cache (percorsi dalla root del sito)
